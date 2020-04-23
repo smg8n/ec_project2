0a38760081cc825c1089375d126ff8e7db5dff98
smg8n <60581192+smg8n@users.noreply.github.com>
2020-04-22 19:37:51 -0500

    Update ga_util.cpp

--- ga_util.cpp
+++ ga_util.cpp
@@ -1,11 +1,4 @@
 #include "ga_bitstring.h"
-/**********************************************************************
-  random_boolean()
-  synopsis:	Return TRUE 50% of the time.
-  parameters:
-  return:	none
-  last updated:	16/05/00
- **********************************************************************/
 
 boolean random_boolean(void)
   {

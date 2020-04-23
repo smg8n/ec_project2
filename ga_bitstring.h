35f7f8ef1643eeae47a21c5ca54b892f028b3188
smg8n <60581192+smg8n@users.noreply.github.com>
2020-04-22 21:11:10 -0500

    Update ga_bitstring.h

--- ga_bitstring.h
+++ ga_bitstring.h
@@ -1,41 +1,6 @@
-/**********************************************************************
-  ga_bitstring.h
- **********************************************************************
-
-  ga_bitstring - GAUL's low-level bitstring routines.
-  Copyright ©2001-2003, Stewart Adcock <stewart@linux-domain.com>
-  All rights reserved.
-
-  The latest version of this program should be available at:
-  http://gaul.sourceforge.net/
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.  Alternatively, if your project
-  is incompatible with the GPL, I will probably agree to requests
-  for permission to use the terms of any other license.
-
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY WHATSOEVER.
-
-  A full copy of the GNU General Public License should be in the file
-  "COPYING" provided with this distribution; if not, see:
-  http://www.gnu.org/
-
- **********************************************************************
-
-  Synopsis:     Low-level bitstring handling functions.
-
- **********************************************************************/
-
 #ifndef GA_BITSTRING_H_INCLUDED
 #define GA_BITSTRING_H_INCLUDED
 
-/*
- * Includes.
- */
-//#include "gaul.h"
 
 #include <stdio.h>
 #include <math.h>
